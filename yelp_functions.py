b51e4b1bf69ea18a0095dde91b4e8b712507e062
Ben T <bentlw@gmail.com>
2020-10-10 18:03:53 +0800

    Cleanup code, updated requirements

--- yelp_functions.py
+++ yelp_functions.py
@@ -7,20 +7,7 @@ from numerizer import numerize
 import re
 
 intent = intents_functions.Intents()
-# enq = entities_functions.Enquiry()
-# rec = entities_functions.Recommendation()
-# res = entities_functions.Reservation()
 slot = entities_functions.Slots()
-
-def get_yelp_resto(entity_list):
-    if not entity_list: return "Sorry I can't understand you :(("
-    value = entity_list[0]
-    column = entity_list[1]
-    
-    if column == 'restaurant.type.food':
-        result = 'Some delicious {} coming right up!'.format(value)
-    else: result = "Sorry, we don't support that just yet!"
-    return result
 
 def bot_reset():
     slot.clear_slots()
